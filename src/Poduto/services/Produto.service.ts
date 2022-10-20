d78615444b950dda70ba5dee72f0a55d7de863be
Daniel Correa <daniel.correa1907@hotmail.com>
2022-10-20 11:18:18 -0300

    doc. produto service

--- src/Poduto/services/Produto.service.ts
+++ src/Poduto/services/Produto.service.ts
@@ -4,14 +4,21 @@ import { CategoriaService } from 'src/Categoria/services/categoria.service';
 import { DeleteResult, ILike, Repository } from 'typeorm';
 import { Produto } from '../entities/Produtos.entity';
 
+/**
+ *  indica que a classe é do tipo Service (Classe de Serviço), 
+que pode ser Injetada em outras Classes através da Injeção de Dependências.
+ */
 @Injectable()
 export class ProdutoService {
   constructor(
     @InjectRepository(Produto)
     private produtoRepository: Repository<Produto>,
     private categoriaService: CategoriaService,
-  ) {}
-
+  ) { }
+  /**
+   * @disc Consulta todos os produtos do banco de dados
+   * @returns todos os produtos que estão no banco de dados
+   */
   async findAll(): Promise<Produto[]> {
     return await this.produtoRepository.find({
       relations: {
@@ -19,6 +26,11 @@ export class ProdutoService {
       },
     });
   }
+  /**
+   * @disc Consulta o produto por id
+   * @param id Identificador para consultar o produto por id
+   * @returns O produto com o id
+   */
   async findById(id: number): Promise<Produto> {
     const produto = await this.produtoRepository.findOne({
       where: {
@@ -32,6 +44,11 @@ export class ProdutoService {
       throw new HttpException('Produto não encontrada', HttpStatus.NOT_FOUND);
     return produto;
   }
+  /**
+   * @disc Consulta o produto por nome
+   * @param nome Identeificador para consultar o produto por nome
+   * @returns O produto com o nome do identificador
+   */
   async findByNome(nome: string): Promise<Produto[]> {
     return await this.produtoRepository.find({
       where: {
@@ -42,6 +59,11 @@ export class ProdutoService {
       },
     });
   }
+  /**
+   * @desc Cria um novo Produto
+   * @param produto Identificador para criar um novo produto
+   * @returns O produto criado
+   */
   async create(produto: Produto): Promise<Produto> {
     if (produto.categoria) {
       const categoria = await this.categoriaService.findById(
@@ -56,6 +78,12 @@ export class ProdutoService {
     }
     return await this.produtoRepository.save(produto);
   }
+  /**
+   * @desc Atualiza o Produto no banco de dados
+   * @param produto.id Identificador para atualizar o produto
+   * @returns O conteudo atualizado
+   * @throws HttpExeption Caso o produto informado não seja encontrado
+   */
   async update(produto: Produto): Promise<Produto> {
     const buscaProduto = await this.findById(produto.id);
 
@@ -75,6 +103,15 @@ export class ProdutoService {
     }
     return await this.produtoRepository.save(produto);
   }
+   /**
+   * @desc Apaga um Produto do banco de dados
+   * @param id O identificador do Produto a ser apagado
+   * @returns Conteúdo vazio
+   * @throws HttpException Caso o id informado não seja encontrado
+   * @example
+   * delete(1); // Será apagado o produto com id = 1
+   * delete(4); // Será apagado o produto com id = 4
+   */
   async delete(id: number): Promise<DeleteResult> {
     const buscaProduto = await this.findById(id);
 
